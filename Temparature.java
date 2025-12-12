de0b56f76135694f8f06380d41a827c81bf390d0
Hasib <gudhulibela031@gmail.com>
2025-12-13 02:33:32 +0600

    Add files via upload

---
+++ Temparature.java
@@ -0,0 +1,12 @@
+//1.Write a programe input from user and print
+import java.util.Scanner;
+public class Temparature {
+	public static void main(String[] args)
+	{
+		Scanner maww=new Scanner(System.in);
+		System.out.println("Enter an intejer nymber: ");
+		int num=maww.nextInt();
+		System.out.println("You entered : "+num);
+	}
+	
+}
